75d1667f24ab1e3e868b6b2e7fe840f531ade688
VatsalSaxena3 <23803988-VatsalSaxena3@users.noreply.replit.com>
2023-12-31 23:55:24 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+function search() {
+  var query = document.getElementById("search-query").value;
+  // Implement your search functionality here, e.g., using AJAX or redirecting to a search results page
+  window.location.href = "https://www.myntra.com/" + query; // Example redirect to Myntra search results page
